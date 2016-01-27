9eda80353bb6a62987f66697104f496d43885a5f
isabelle george and taylor pokoj <10izzygeorge@gmail.com>
2016-01-27 09:19:41 -0800

    Add initial files and pass first spec.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var randomWord = function(){
+  var words = ["orange", "coffee", "onomatopoeia", "dangerous", "grandiose", "scintillating", "capricious", "whimsical", "pizzas", "demonic"]
+  var randomWords = words[Math.floor(words.length * Math.random())];
+  return true;
+ }
---
+++ specs/specs.js
@@ -0,0 +1,8 @@
+describe('randomWord', function() {
+  it("generates a random word from an array", function() {
+    expect(randomWord("orange")).to.equal(true);
+  });
+  // it("splits up random word into an array of letters", function() {
+  //   expect(randomWord("orange")).to.eql(["o", "r", "a", "n", "g", "e"]);
+  // });
+});
